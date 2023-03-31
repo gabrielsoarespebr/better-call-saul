1f8fdcd4d169c02e4209f776b1111421515076d9
gabrielsoarespebr <gabrielsoares.pe.br@gmail.com>
2023-03-31 11:53:56 -0300

    style: Reset CSS

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,13 +6,13 @@ import style from "./style.module.css"
 export const Header = () => {
     return (
         <header className="d-flex">
-            <img className="col-5" src={"https://upload.wikimedia.org/wikipedia/pt/thumb/8/8a/Better_Call_Saul_logo.svg/1200px-Better_Call_Saul_logo.svg.png"} alt="Better Call Saul Logo" />
+            <img className={`col-5 mx-5 ${style.logo}`} src={"https://upload.wikimedia.org/wikipedia/pt/thumb/8/8a/Better_Call_Saul_logo.svg/1200px-Better_Call_Saul_logo.svg.png"} alt="Better Call Saul Logo" />
             <img className="col-5" src={"./src/assets/images/SaulGoodmanArt.png"} alt="Saul Goodman Art" />
 
             <Navbar expand="lg">
                 <Container>
                     {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link href="#home">Home</Nav.Link>
