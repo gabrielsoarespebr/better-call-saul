874bb77ec19727a3185c21cbf321a808d5773f8f
gabrielsoarespebr <gabrielsoares.pe.br@gmail.com>
2023-04-03 16:24:17 -0300

    refactor: Footer with speech bubble

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,24 +2,46 @@ import style from "./style.module.css"
 
 export function Footer() {
     return (
-        <footer className='text-center py-3 mb-0'>
-            <p className='m-0 fs-5'>
-                Desenvolvido por <a className='text-reset text-decoration-none' href="https://github.com/gabrielsoarespebr/better-call-saul" target={'_blank'}>Gabriel Soares</a> &#x2022; {new Date().getFullYear()}
-            </p>
-            <p>Você também pode se interessar em:</p>
-            <ul className={`list-unstyled d-flex justify-content-center gap-3 ${style.others}`}>
-                <li>
-                    <a href="http://www.savewalterwhite.com" target="_blank">
-                        <img src={"./src/assets/images/SaveWalterWhite.png"} alt="Save Walter White" title="Save Walter White" />
-                    </a>
-                </li>
-                <li>
-                    <a href="http://www.lphishiring.com" target="_blank">
-                        <img src={"./src/assets/images/LosPollosHermanos.png"} alt="Los Pollos Hermanos" title="Los Pollos Hermanos" />
-                    </a>
-                </li>
-                <li></li>
-            </ul>
+        <footer className='d-flex justify-content-between gap-1 text-black lh-1'>
+            <div className={style.speechBubble}>
+                <p>E aí, pessoal!? Sou o melhor advogado que vocês podem encontrar em Albuquerque: Saul Goodman!</p>
+                <p>Tô aqui pra falar de um cara tão bom quanto eu, o gênio que criou essa maravilha de site que vocês tão navegando agora mesmo. Eu desconfio que ele seja mais um daqueles hackers que em breve será preso pelo FBI, mas olha, tenho que admitir, tô impressionado com o trabalho dele!</p>
+                <div className="d-flex justify-content-around gap-3">
+                    <div className="col-5 text-center">
+                        <p>Contrate-o antes que ele seja preso:</p>
+                        <ul className={`list-unstyled d-flex justify-content-center gap-3 m-0 ${style.btnRedirect}`}>
+                            <li>
+                                <a href="https://www.linkedin.com/in/gabrielsoarespebr/" target="_blank">
+                                    <img src={"./src/assets/icons/LinkedinCircle.png"} alt="Gabriel Soares - LinkedIn" title="Gabriel Soares - LinkedIn" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/gabrielsoarespebr/better-call-saul" target="_blank">
+                                    <img src={"./src/assets/icons/GithubCircle.png"} alt="Gabriel Soares - Github" title="Gabriel Soares - Github" />
+                                </a>
+                            </li>
+                            <li></li>
+                        </ul>
+                    </div>
+                    <div className="col-5 text-center">
+                        <p>Você também pode se interessar em:</p>
+                        <ul className={`list-unstyled d-flex justify-content-center gap-3 m-0 ${style.btnRedirect}`}>
+                            <li>
+                                <a href="http://www.savewalterwhite.com" target="_blank">
+                                    <img src={"./src/assets/icons/SaveWalterWhite.png"} alt="Save Walter White" title="Save Walter White" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://www.lphishiring.com" target="_blank">
+                                    <img src={"./src/assets/icons/LosPollosHermanos.png"} alt="Los Pollos Hermanos" title="Los Pollos Hermanos" />
+                                </a>
+                            </li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <img className={`col-3 ${style.saulSpeaking}`} src={"./src/assets/images/SaulGoodmanOnDesk.png"} alt="Saul Goodman On Desk" />
         </footer>
     )
 }
