171f9bf0c2a9990ca2a244d9d5d26639b372ec68
gabrielsoarespebr <gabrielsoares.pe.br@gmail.com>
2023-04-23 12:15:09 -0300

    refactor: About text change

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -15,9 +15,9 @@ export const About = () => {
             <img src={"./src/assets/images/SaulGoodmanSide.png"} alt="Saul Goodman" />
             <div style={{ fontSize: "110%" }} className="m-5 align-self-center">
                 <h2 className="mb-4">Sobre Saul Goodman</h2>
-                <p>Com a carreira jurídica iniciada na Defensoria Pública de Cook County, tornou-se conhecido pelo seu jeito eficaz e não convencional de advogar, visando <span style={emphasisText}>prevenção e remediação de danos</span>.</p>
+                <p>Com a carreira jurídica iniciada na Defensoria Pública de Cook County, tornou-se conhecido pelo seu jeito eficaz e não convencional de advogar, visando <span style={emphasisText}>prevenção e reparação de danos</span>.</p>
                 <p>Hoje, seu escritório é referência na arte de defender, com <span style={emphasisText}>experiência comprovada em diversas áreas do Direito</span> e atendimento a uma <span style={emphasisText}>vasta gama de clientes</span>, dos mais abastados aos menos favorecidos.</p>
-                <p>Dentre os serviços mais procurados, estão:</p>
+                <p>Dentre os serviços mais procurados, destacam-se:</p>
                 <ul style={{ height: "10vh" }} className="d-flex   list-unstyled">
                     <LawService serviceIcon={"./src/assets/icons/servicesDivorce.png"} serviceName="Divórcio" />
                     <LawService serviceIcon={"./src/assets/icons/servicesDamage.png"} serviceName="Dano moral e material" />
