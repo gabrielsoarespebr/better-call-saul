bf70d7ac895715092447d937dddbe127d1204c3c
gabrielsoarespebr <gabrielsoares.pe.br@gmail.com>
2023-04-23 13:52:05 -0300

    clean: Header jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import style from "./style.module.css"
 export const Header = () => {
     return (
